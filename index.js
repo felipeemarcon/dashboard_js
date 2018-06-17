1174abde2e52f47d3e9d31e2ff31596c4bc53db3
Felipe Marcon <felipeemarcon@gmail.com>
2018-06-17 13:30:58 -0300

    Comecando Controllers e Models

--- index.js
+++ index.js
@@ -1,26 +1,59 @@
-let name = document.querySelector('#exampleInputName');
-let gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
-let birth = document.querySelector('#exampleInputBirth');
-let country = document.querySelector('#exampleInputCountry');
-let email = document.querySelector('#exampleInputEmail');
-let password = document.querySelector('#exampleInputPassword');
-let photo = document.querySelector('#exampleInputFile');
-let admin = document.querySelector('#exampleInputAdmin');
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
-let fields = document.querySelectorAll("#form-user-create [name]");
+function addLine(dataUser) {
 
-fields.forEach(function(field, index){
+  console.log(dataUser);
 
-  if (field.name == "gender") {
+  document.getElementById("table-users").innerHTML = `
+    <tr>
+      <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+      <td>${dataUser.name}</td>
+      <td>${dataUser.email}</td>
+      <td>${dataUser.admin}</td>
+      <td>${dataUser.birth}</td>
+      <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+      </td>
+    </tr>
+  `;
+  
+}
 
-    if (field.checked) {
-      console.log("Sim", field)
+document.getElementById("form-user-create").addEventListener("submit", function(event) {
+
+  event.preventDefault();
+
+  fields.forEach(function (field, index) {
+
+    if (field.name == "gender") {
+
+      if (field.checked) {
+
+        user[field.name] = field.value;
+
+      }
+
+    } else {
+
+      user[field.name] = field.value;
+
     }
 
-  } else {
+  });
 
-    console.log("Não")
+  var objectUser = new User(
+    user.name, 
+    user.gender, 
+    user.birth, 
+    user.country, 
+    user.email, 
+    user.password, 
+    user.photo, 
+    user.admin
+  );
 
-  }
+  addLine(objectUser);
 
 });
