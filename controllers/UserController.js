3120bb2eb7c6a3903f65748aad50909c5dc42934
Felipe Marcon <felipeemarcon@gmail.com>
2018-06-25 21:44:23 -0300

    armazenando dados com localStorage (era sessionStorage)

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -217,10 +217,10 @@ class UserController {
 
     let users = [];
 
-    if (sessionStorage.getItem("users")) {
+    if (localStorage.getItem("users")) {
 
-      users = JSON.parse(sessionStorage.getItem("users"));
-      
+      users = JSON.parse(localStorage.getItem("users"));
+
     }
 
     return users;
@@ -248,7 +248,9 @@ class UserController {
 
     users.push(data);
 
-    sessionStorage.setItem("users", JSON.stringify(users));
+    // sessionStorage.setItem("users", JSON.stringify(users));
+
+    localStorage.setItem("users", JSON.stringify(users));
 
   }
 
