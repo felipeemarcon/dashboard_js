669fb82dfb0e9b8b1a98912a73d121875b90ba1e
Felipe Marcon <felipeemarcon@gmail.com>
2018-06-25 21:40:43 -0300

    Excluindo dados e armazenando dados com localStorage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,6 +8,7 @@ class UserController {
 
     this.onSubmit();
     this.onEdit();
+    this.selectAll();
 
   }
 
@@ -30,8 +31,6 @@ class UserController {
 
       let values = this.getValues(this.formUpdateEl);
 
-      console.log(values);
-
       let index = this.formUpdateEl.dataset.trIndex
 
       let tr = this.tableEl.rows[index];
@@ -45,6 +44,7 @@ class UserController {
 
           if (!values.photo) {
             result._photo = userOld._photo;
+
           } else {
             result._photo = content;
           }
@@ -102,6 +102,8 @@ class UserController {
         (content) => {
 
           values.photo = content;
+
+          this.insert(values);
 
           this.addLine(values);
 
@@ -211,6 +213,45 @@ class UserController {
 
   }
 
+  getUsersStorage(){
+
+    let users = [];
+
+    if (sessionStorage.getItem("users")) {
+
+      users = JSON.parse(sessionStorage.getItem("users"));
+      
+    }
+
+    return users;
+  }
+
+  selectAll(){
+
+    let users = this.getUsersStorage();
+    
+    users.forEach(dataUser => {
+
+      let user = new User();
+
+      user.loadFromJSON(dataUser);
+
+      this.addLine(user);
+
+    });
+    
+  }
+
+  insert(data){
+
+    let users = this.getUsersStorage();
+
+    users.push(data);
+
+    sessionStorage.setItem("users", JSON.stringify(users));
+
+  }
+
   addLine(dataUser) {
 
     let tr = document.createElement('tr');
@@ -225,7 +266,7 @@ class UserController {
       <td>${Utils.dateFormat(dataUser.register)}</td>
       <td>
         <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
       </td>
   `;
 
@@ -238,9 +279,23 @@ class UserController {
   }
 
   addEventsTr(tr){
+
+    tr.querySelector(".btn-delete").addEventListener("click", e => {
+
+      if(confirm("Deseja realmente excluir?")){
+
+        tr.remove();
+
+        this.updateCount();
+
+      }
+
+    });
+
     tr.querySelector(".btn-edit").addEventListener("click", e => {
 
       let json = JSON.parse(tr.dataset.user);
+
       this.formUpdateEl.dataset.trIndex = tr.sectionRowIndex;
 
       for (let name in json) {
